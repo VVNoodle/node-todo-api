5743f4a24352dee8cb713afb2991bd2862154ae6
Egan <ebisma@ucsc.edu>
2017-05-15 02:12:53 -0700

    Added mocha test files for POST request of server

--- server/server.js
+++ server/server.js
@@ -18,10 +18,12 @@ app.post('/todos', (req,res)=>{
   todo.save().then((result)=>{
     res.send(result);
   }, (err)=>{
-    res.status(400).send('ERROR SENDING TODO, ', err);
+    res.send(400, err);
   });
 });
 
 app.listen(3000, ()=>{
   console.log('Started on port 3000!');
 })
+
+module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,50 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done)=>{
+  Todo.remove({}).then(()=>done());
+});
+
+describe('POST /todos', ()=>{
+  it('should create a new todo', (done)=>{
+    var todo = 'Test todo text';
+    request(app)
+      .post('/todos')
+      .send({todo})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo).toBe(todo);
+      })
+      .end((err, res)=>{
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos)=>{
+          expect(todos.length).toBe(1);
+          expect(todos[0].todo).toBe(todo);
+          done();
+        }).catch((err) => done(err));
+      });
+  })
+
+  it('should not add todo w/ bad data', (done)=>{
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res)=>{
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos)=>{
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((err) => done(err));
+      });
+  })
+});
