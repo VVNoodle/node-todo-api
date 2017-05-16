19b3e27e9bfd23a8ef489226059b6446be51c850
Egan <ebisma@ucsc.edu>
2017-05-16 01:30:17 -0700

    Modified project for Heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@
 //  C:\Users\Brick\mongo-data
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 module.exports = {mongoose};
 
-// module.exports.mongoose = mongoose; 
+// module.exports.mongoose = mongoose;
