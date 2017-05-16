f8a9efd0bbfc3415eab9992fd784e09c9193ebeb
Egan <ebisma@ucsc.edu>
2017-05-15 20:42:14 -0700

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {ObjectID} = require('mongodb');
 
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -20,7 +21,7 @@ app.post('/todos', (req,res)=>{
   }, (err)=>{
     res.send(400, err);
   });
-}); //end POST
+}); //end POST /todos
 
 app.get('/todos', (req, res)=>{
   Todo.find().then((data)=>{
@@ -28,7 +29,27 @@ app.get('/todos', (req, res)=>{
   }).catch((err)=>{
     res.send(400, err);
   });
-});//end GET
+});//end GET /todos
+
+app.get('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(
+    (doc)=>{
+      if(!doc){
+        return res.status(404).send();
+      }
+      res.send({todo: doc});
+    },
+    (err)=>{
+      res.status(404).send();
+    }
+  )
+}); //end GET /todos/:id
 
 app.listen(3000, ()=>{
   console.log('Started on port 3000!');
