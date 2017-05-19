5a116410d92c0a56fb7bf5049afd6d92769b30f9
VVNoodle <brickkace@gmail.com>
2017-05-19 00:32:00 -0700

    added config to take care of process.env.NODE_ENV and process.env.MONGODB_URI, also changed some lines in mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,6 @@
 //  mongod.exe --dbpath D:\mongo-data
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = {mongoose};
-
 // module.exports.mongoose = mongoose;
